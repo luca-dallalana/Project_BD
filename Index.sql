1fe11e98edac6e77954b1197527436086c5968af
luca-dallalana <luca.dallalana@tecnico.ulisboa.pt>
2024-05-23 17:47:34 +0100

    INDEX

--- Index.sql
+++ Index.sql
@@ -27,12 +27,6 @@ CREATE INDEX nif_idx ON consulta(nif);
 -- A especialidade e a qtd aceleram a seleção e as operações GROUP BY e SORT
 CREATE INDEX data_ssn_esp_qtd_idx ON receita(data, codigo_ssn, especialidade, qtd);
 
-May 23, 2024
-vista
-
-
-May 23, 2024
-indices
 SELECT especialidade, SUM(quantidade) AS qtd FROM medico
 JOIN consulta USING (nif)
 JOIN receita USING (codigo_ssn) 
