c91fcadf449456cd115a421c773333a08d4f50df
luca-dallalana <luca.dallalana@tecnico.ulisboa.pt>
2024-05-30 17:05:00 +0100

    correcao Index

--- Index.sql
+++ Index.sql
@@ -1,8 +1,10 @@
 -- Indices
 
+%%sql
+-- CREATE INDEX ...
 -- Aceleram as operações de JOIN
-CREATE INDEX ssn_idx ON consulta(ssn);
-CREATE INDEX id_idx ON observacao(id);
+CREATE INDEX idx_paciente_consulta_ssn ON paciente(ssn), consulta(ssn);
+CREATE INDEX idx_consulta_observacao_id ON consulta(id), observacao(id);
 
 -- Um índice composto (parametro, valor) permite filtrar rapidamente as linhas que atendem a ambos os critérios
 -- É mais eficiente do que usar dois índices separados para cada coluna
@@ -13,19 +15,18 @@ SELECT nome
 FROM paciente
 JOIN consulta USING (ssn)
 JOIN observacao USING (id)
-WHERE parametro = ‘pressão diastólica’ AND valor >= 9;
+WHERE parametro = 'pressão diastólica' AND valor >= 9;
 
 
--- Melhora no agrupamento por especialidade
-CREATE INDEX especialidade_idx ON medico(especialidade);
+-- CREATE INDEX ...
 
 -- Acelera a operação JOIN
-CREATE INDEX nif_idx ON consulta(nif);
+CREATE INDEX idx_medico_consulta_nif ON medico(nif), consulta(nif);
+CREATE INDEX idx_consulta_receita_codigo_sns ON consulta(codigo_sns), receita(codigo_sns);
 
--- O Índice composto em data e codigo_ssn acelera a filtração baseada na data
--- O codigo_ssn acelera a operação JOIN
 -- A especialidade e a qtd aceleram a seleção e as operações GROUP BY e SORT
-CREATE INDEX data_ssn_esp_qtd_idx ON receita(data, codigo_ssn, especialidade, qtd);
+CREATE INDEX idx_medico_especialidade ON medico(especialidade);
+CREATE INDEX idx_consulta_data ON consulta(data);
 
 SELECT especialidade, SUM(quantidade) AS qtd FROM medico
 JOIN consulta USING (nif)
