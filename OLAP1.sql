69e6ec75b52541346930d76543cd116c4cfd298e
pballe-s <ballesanguinetti@gmail.com>
2024-05-31 13:33:20 +0100

    OLAP1_atualizado

--- OLAP1.sql
+++ OLAP1.sql
@@ -1,40 +1,15 @@
-WITH orthopedic_appointments AS (
-    SELECT
-        c.ssn,
-        c.id AS consulta_id,
-        o.parametro,
-        o.valor
-    FROM
-        consulta c
-    JOIN medico m ON c.nif = m.nif
-    JOIN observacao o ON c.id = o.id
-    WHERE
-        m.especialidade = 'ortopedia'
-),
-grouped_appointments AS (
-    SELECT
-        ssn,
-        parametro,
-        valor,
-        array_agg(consulta_id) AS consulta_ids
-    FROM
-        orthopedic_appointments
-    GROUP BY
-        ssn, parametro, valor
-),
-appointments_with_dates AS (
-    SELECT
-        ga.ssn,
-        ga.parametro,
-        ga.valor,
-        ga.consulta_ids,
-        array_agg(c.data ORDER BY c.data) AS consulta_dates
-    FROM
-        grouped_appointments ga
-    JOIN consulta c ON c.id = ANY(ga.consulta_ids)
-    GROUP BY
-        ga.ssn, ga.parametro, ga.valor, ga.consulta_ids
-),
+WITH hp_appointments_with_dates AS(
+	SELECT
+		hp.ssn,
+		hp.chave,
+		hp.valor,
+		array_agg(hp.id) AS consulta_ids,
+		array_agg(hp.data ORDER BY hp.data) AS consulta_dates
+	FROM
+		historial_paciente hp
+	WHERE hp.tipo = 'observacao' AND hp.especialidade = 'ortopedia'
+	GROUP BY hp.ssn, hp.chave, hp.valor, consulta_ids
+)
 max_intervals AS (
     SELECT
         ssn,
@@ -44,7 +19,7 @@ max_intervals AS (
         consulta_dates,
         MAX(c2.data - c1.data) OVER (PARTITION BY ssn, parametro, valor) AS max_interval
     FROM
-        appointments_with_dates,
+        hp_appointments_with_dates,
         LATERAL unnest(consulta_dates) WITH ORDINALITY AS c1(data, ord1),
         LATERAL unnest(consulta_dates) WITH ORDINALITY AS c2(data, ord2)
     WHERE
